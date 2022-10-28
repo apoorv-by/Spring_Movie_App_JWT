1eaa4ff6f849eb47f4535c2192629a1b84c52d23
1034510 <Apoorv.Bhardwaj@blueyonder.com>
2022-10-28 12:21:10 +0530

    Initial commit

---
+++ src/main/java/com/movie/jwt/spring/movieJwtSpring/model/Movie.java
@@ -0,0 +1,25 @@
+package com.movie.jwt.spring.movieJwtSpring.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+import java.util.Date;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Movie {
+    @JsonSerialize(using = ToStringSerializer.class)
+    private ObjectId id;
+    private String name;
+    private String releaseDate;
+}
